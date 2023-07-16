fd7fac0e0d0752c71c872b0ac0b62f1e77f1ff75
Katty <kattystc@gmail.com>
2023-07-16 02:02:07 +0000

    Actualizado

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :commets, dependent: :destroy
   has_many :reactions, dependent: :destroy
+  #accepts_nested_attributes_for :commets, reject_if: :no_name_category
 
   Kinds = %w[like dislike].freeze
 end
